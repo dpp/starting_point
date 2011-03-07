e47ef1a1d49a961f422ca4559818b2edcd60597d
David Pollak <feeder.of.the.bears@gmail.com>
2011-03-07 04:00:53 -0800

    Code for the very simplest REST service

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -12,7 +12,6 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
 
   override def libraryDependencies = Set(
     "net.liftweb" %% "lift-webkit" % liftVersion % "compile",
-    "net.liftweb" %% "lift-mapper" % liftVersion % "compile",
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test",
     "junit" % "junit" % "4.5" % "test",
     "ch.qos.logback" % "logback-classic" % "0.9.26",
