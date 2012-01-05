2741edb8ba973606480acec3b432b5655342ffd0
David Pollak <feeder.of.the.bears@gmail.com>
2012-01-05 08:58:06 -0800

    Updated to Lift 2.4 and Scala 2.9.1

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  val liftVersion = "2.4-RC1"
+  val liftVersion = "2.4"
 
   // uncomment the following if you want to use the snapshot repo
   //  val scalatoolsSnapshot = ScalaToolsSnapshots
@@ -16,7 +16,7 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test",
     "junit" % "junit" % "4.5" % "test",
     "ch.qos.logback" % "logback-classic" % "0.9.26",
-    "org.scala-tools.testing" %% "specs" % "1.6.6" % "test",
+    "org.scala-tools.testing" %% "specs" % "1.6.9" % "test",
     "com.h2database" % "h2" % "1.2.138"
   ) ++ super.libraryDependencies
 }
